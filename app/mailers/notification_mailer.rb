b62d437d8dbc7d33052bad3eabfa3cf83949c4b4
Kyle Loveless <lovelessk999@gmail.com>
2019-09-16 00:06:58 +0000

    42: Adjusting the mailer non working

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "testuser979575@gmail.com",
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
           subject: "A comment has been added to your place.")
   end
 
